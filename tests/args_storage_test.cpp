3007133140a39ed47bc14f90c887fe8ad5dfe1d2
Vincent <91033856+Vinwcent@users.noreply.github.com>
2024-12-30 22:47:06 +0100

    feat: add args storage

---
+++ tests/args_storage_test.cpp
@@ -0,0 +1,32 @@
+#include "src/args_storage.hpp"
+
+#include <gtest/gtest.h>
+
+#include "src/function_wrappers.hpp"
+
+TEST(ArgsStorageTest, construction) {
+  vvw_gen_lib::ArgsStorage<int, int> storage(5, 4);
+  std::tuple<int, int> containedTuple = storage.getArgsTuple();
+  EXPECT_EQ(std::get<0>(containedTuple), 5);
+  EXPECT_EQ(std::get<1>(containedTuple), 4);
+
+  std::vector<void*> data = storage.getArgPtrs();
+  EXPECT_EQ(*static_cast<int*>(data[0]), 5);
+  EXPECT_EQ(*static_cast<int*>(data[1]), 4);
+}
+
+TEST(ArgsStorageTest, usageInFunctionWrapper) {
+  int value = 3;
+  std::function<void(int)> valueAdd(
+      [&value](int increment) { value += increment; });
+  vvw_gen_lib::FunctionWrapper<int> wrapper(valueAdd);
+
+  std::vector<std::unique_ptr<vvw_gen_lib::FunctionWrapperTypeEraser>>
+      erasedTypeWrapper;
+  erasedTypeWrapper.push_back(
+      std::make_unique<vvw_gen_lib::FunctionWrapper<int>>(valueAdd));
+
+  vvw_gen_lib::ArgsStorage<int> storage(3);
+  (*erasedTypeWrapper[0])(storage.getArgPtrs());
+  EXPECT_EQ(value, 6);
+}
